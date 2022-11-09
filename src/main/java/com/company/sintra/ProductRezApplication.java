c39948ecb0b06cec428f47d1d9efd1ebd34c737c
AliHsynvv <gsynvali@gmail.com>
2022-11-09 11:37:37 +0400

    java project is created

---
+++ src/main/java/com/company/sintra/ProductRezApplication.java
@@ -0,0 +1,13 @@
+package com.company.sintra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRezApplication.class, args);
+    }
+
+}
